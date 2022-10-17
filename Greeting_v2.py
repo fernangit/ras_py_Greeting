d83e29e26c6c595e7e19c7d54416e5afde23e9ad
fernangit <morio.tsunaki@gmail.com>
2022-10-17 09:06:11 +0900

    modify

--- Greeting_v2.py
+++ Greeting_v2.py
@@ -21,6 +21,7 @@ mng_lst = ['ういーす',
            'おいーす',
            'おはようぐると',
            'ざいまーす',
+           'ざいますっ',
            'もーにん',
            'おはよお',
            'ざーす']
@@ -112,7 +113,7 @@ img_resize = cv.resize(img, (640, 365))
 cv.imshow("M's Aisatsu Unit", img_resize)
 cv.moveWindow("M's Aisatsu Unit", -35, -3) 
 
-jtalk.jtalk('えむず　あいさつユニット 起動しまーーーすっ')
+jtalk.jtalk('えむず　あいさつユニット しどうっ')
 d = datetime.now()
 nxt_h = d.hour
 nxt_m = random.randint(0, 59)
@@ -141,6 +142,12 @@ with picamera.PiCamera() as camera:
         face_cascade = cv.CascadeClassifier('haarcascades/haarcascade_frontalface_default.xml')
         # 目検出のための学習元データを読み込む
         eye_cascade = cv.CascadeClassifier('haarcascades/haarcascade_eye.xml')
+        # 体検出のための学習元データを読み込む
+        body_cascade = cv.CascadeClassifier('haarcascades/haarcascade_fullbody.xml')
+        # 上半身検出のための学習元データを読み込む
+        upperbody_cascade = cv.CascadeClassifier('haarcascades/haarcascade_upperbody.xml')
+        # 下半身検出のための学習元データを読み込む
+        lowerbody_cascade = cv.CascadeClassifier('haarcascades/haarcascade_lowerbody.xml')
 
         while True:
             # カメラから映像を取得する（OpenCVへ渡すために、各ピクセルの色の並びをBGRの順番にする）
@@ -148,16 +155,25 @@ with picamera.PiCamera() as camera:
             # 顔検出の処理効率化のために、写真の情報量を落とす（モノクロにする）
             grayimg = cv.cvtColor(stream.array, cv.COLOR_BGR2GRAY)
 
-            # 顔検出を行う
-            facerect = face_cascade.detectMultiScale(grayimg, scaleFactor=1.2, minNeighbors=2, minSize=(100, 100))
+#            # 顔検出を行う
+#            facerect = face_cascade.detectMultiScale(grayimg, scaleFactor=1.2, minNeighbors=2, minSize=(100, 100))
             # 目検出を行う
-            eyerect = eye_cascade.detectMultiScale(grayimg)
-            
-            # 顔が検出された場合
-            if len(facerect) > 0:
-                # 検出した場所すべてに赤色で枠を描画する
-                for rect in facerect:
-                    cv.rectangle(stream.array, tuple(rect[0:2]), tuple(rect[0:2]+rect[2:4]), (0, 0, 255), thickness=3)
+            cv.rectangle(stream.array, (100,70), (400,300), (0, 0, 255), 2)
+            eye_grayimg = grayimg[70:300, 100:400]
+            eyerect = eye_cascade.detectMultiScale(eye_grayimg, minNeighbors=6, minSize=(20,20))
+#            eyerect = eye_cascade.detectMultiScale(grayimg, minNeighbors=6)
+#            # 体検出を行う
+#            bodyrect = body_cascade.detectMultiScale(grayimg, scaleFactor=1.1, minNeighbors=3, minSize=(40,40))
+            # 上半身検出を行う
+            upperbodyrect = upperbody_cascade.detectMultiScale(grayimg, scaleFactor=1.1, minNeighbors=3, minSize=(40,40))
+#            # 下半身検出を行う
+#            lowerbodyrect = lowerbody_cascade.detectMultiScale(grayimg, scaleFactor=1.1, minNeighbors=3, minSize=(40,40))
+
+#            # 顔が検出された場合
+#            if len(facerect) > 0:
+#                # 検出した場所すべてに赤色で枠を描画する
+#                for rect in facerect:
+#                    cv.rectangle(stream.array, tuple(rect[0:2]), tuple(rect[0:2]+rect[2:4]), (0, 0, 255), thickness=3)
 
             # 目を検出した場合
             if len(eyerect) > 0:
@@ -165,14 +181,36 @@ with picamera.PiCamera() as camera:
                 for rect in eyerect:
                     cv.rectangle(stream.array, tuple(rect[0:2]), tuple(rect[0:2]+rect[2:4]), (0, 255, 0), thickness=3)
 
+#            # 体を検出した場合
+#            if len(bodyrect) > 0:
+#                # 検出した場所すべてに緑色で枠を描画する
+#                for rect in bodyrect:
+#                    cv.rectangle(stream.array, tuple(rect[0:2]), tuple(rect[0:2]+rect[2:4]), (255, 0, 0), thickness=3)
+
+            # 上半身を検出した場合
+            if len(upperbodyrect) > 0:
+                # 検出した場所すべてに緑色で枠を描画する
+                for rect in upperbodyrect:
+                    cv.rectangle(stream.array, tuple(rect[0:2]), tuple(rect[0:2]+rect[2:4]), (255, 0, 0), thickness=3)
+
+#            # 下半身を検出した場合
+#            if len(lowerbodyrect) > 0:
+#                # 検出した場所すべてに緑色で枠を描画する
+#                for rect in lowerbodyrect:
+#                    cv.rectangle(stream.array, tuple(rect[0:2]), tuple(rect[0:2]+rect[2:4]), (255, 0, 0), thickness=3)
+
             # 結果の画像を表示する
 #            cv.imshow('camera', stream.array)
             img_resize = cv.resize(stream.array, (100, 100))
             cv.imshow("camera", img_resize)
             cv.moveWindow("camera", 0, 300) 
-
-            # 顔か目を検出したら挨拶
-            if len(facerect) > 0 or len(eyerect) > 1:
+#
+#            # 顔か目を検出したら挨拶
+#            if len(facerect) > 0 or len(eyerect) > 1:
+            # 体を検出したら挨拶
+#            if len(bodyrect) > 0 or len(upperbodyrect) > 0 or len(lowerbodyrect) > 0:
+#            if len(bodyrect) > 0 or len(upperbodyrect) > 0:
+            if len(eyerect) > 1 or len(upperbodyrect) > 0:
                 if (time.time() - t_st) > 5:
                     #前回から5秒以上経過していたら挨拶
                     cmd = "omxplayer 受付.mp4"
